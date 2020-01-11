dbcd21cb0e08b3c0fd58a5ce65bfd8e333064f7c
kit_angel <kit.angelov@gmail.com>
2020-01-11 18:14:20 +0300

    auth

--- lib/screens/auth/phone_input.dart
+++ lib/screens/auth/phone_input.dart
@@ -2,32 +2,16 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 
-void main() => runApp(MyApp());
+class AuthPhoneInputScreen extends StatefulWidget {
+  AuthPhoneInputScreen({Key key, this.title}) : super(key: key);
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _AuthPhoneInputState createState() => _AuthPhoneInputState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _AuthPhoneInputState extends State<AuthPhoneInputScreen> {
 
   TextEditingController _smsCodeController = TextEditingController();
   TextEditingController _phoneNumberController = TextEditingController();
