6e8e82750e5822e48cb49589fd59d6a7e9e92b1d
kit_angel <kit.angelov@gmail.com>
2020-01-14 17:45:10 +0300

    google maps

--- lib/screens/map/map.dart
+++ lib/screens/map/map.dart
@@ -1,6 +1,12 @@
+import 'dart:core';
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 
 class MapScreen extends StatefulWidget {
@@ -9,35 +15,101 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  String _mapStyle;
+  Completer<GoogleMapController> _controller = Completer();
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
 
-  Widget build(BuildContext context) {
-    return new FlutterMap(
-      options: new MapOptions(
-        center: new LatLng(51.5, -0.09),
-        zoom: 13.0,
-      ),
-      layers: [
-        new TileLayerOptions(
-          urlTemplate: "https://api.mapbox.com/styles/v1/kitangelov/cjzojp5lx0ajr1cntcuwaimnk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia2l0YW5nZWxvdiIsImEiOiJjamd1aHZncTMxMjF6MndtcWdjZGZhY2g1In0.s4vQ4pbKkTCpKt6psOPxMw",
-          additionalOptions: {
-            'accessToken': 'pk.eyJ1Ijoia2l0YW5nZWxvdiIsImEiOiJjamd1aHZncTMxMjF6MndtcWdjZGZhY2g1In0.s4vQ4pbKkTCpKt6psOPxMw',
-            'id': 'mapbox.streets',
-          },
-        ),
-        new MarkerLayerOptions(
-          markers: [
-            new Marker(
-              width: 80.0,
-              height: 80.0,
-              point: new LatLng(51.5, -0.09),
-              builder: (ctx) =>
-              new Container(
-                child: new FlutterLogo(),
-              ),
-            ),
-          ],
-        ),
-      ],
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(51.67204, 39.1843),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  void _add() async{
+    final int targetWidth = 60;
+    final File markerImageFile = await DefaultCacheManager().getSingleFile("https://image.flaticon.com/icons/png/512/65/65000.png");
+    final Uint8List markerImageBytes = await markerImageFile.readAsBytes();
+    
+    final Codec markerImageCodec = await instantiateImageCodec(
+      markerImageBytes,
+      targetWidth: targetWidth,
     );
+    final FrameInfo frameInfo = await markerImageCodec.getNextFrame();
+    final ByteData byteData = await frameInfo.image.toByteData(
+      format: ImageByteFormat.png,
+    );
+
+    final Uint8List resizedMarkerImageBytes = byteData.buffer.asUint8List();
+
+    var markerIdVal = "1";
+    final MarkerId markerId = MarkerId(markerIdVal);
+
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: LatLng(51.67204, 39.1843),
+      infoWindow: InfoWindow(title: markerIdVal, snippet: '*'),
+      icon: BitmapDescriptor.fromBytes(resizedMarkerImageBytes),
+      onTap: () {
+        _onMarkerTapped(markerId);
+      },
+    );
+
+    setState(() {
+      // adding a new marker to map
+      markers[markerId] = marker;
+    });
+  }
+  
+  void _onMarkerTapped(markerId) {
+    print(markerId);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    rootBundle.loadString('assets/map_style.txt').then((string) {
+      _mapStyle = string;
+    });
+    Timer(
+      Duration(seconds: 5),
+      () {
+        setState(() {
+          _add();
+        });
+      }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+          controller.setMapStyle(_mapStyle);
+        },
+        markers: Set<Marker>.of(markers.values),
+        mapToolbarEnabled: false,
+
+        
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
