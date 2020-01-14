6af72a1fe8bf51790f118d8aaf46bd44524d045e
kit_angel <kit.angelov@gmail.com>
2020-01-14 18:05:55 +0300

    adsf

--- lib/screens/map/map.dart
+++ lib/screens/map/map.dart
@@ -1,12 +1,9 @@
 import 'dart:core';
 import 'dart:async';
-import 'dart:io';
-import 'dart:typed_data';
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:eva/screens/map/marker.dart';
 
 
 class MapScreen extends StatefulWidget {
@@ -24,51 +21,6 @@ class _MapScreenState extends State<MapScreen> {
     zoom: 14.4746,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-  void _add() async{
-    final int targetWidth = 60;
-    final File markerImageFile = await DefaultCacheManager().getSingleFile("https://image.flaticon.com/icons/png/512/65/65000.png");
-    final Uint8List markerImageBytes = await markerImageFile.readAsBytes();
-    
-    final Codec markerImageCodec = await instantiateImageCodec(
-      markerImageBytes,
-      targetWidth: targetWidth,
-    );
-    final FrameInfo frameInfo = await markerImageCodec.getNextFrame();
-    final ByteData byteData = await frameInfo.image.toByteData(
-      format: ImageByteFormat.png,
-    );
-
-    final Uint8List resizedMarkerImageBytes = byteData.buffer.asUint8List();
-
-    var markerIdVal = "1";
-    final MarkerId markerId = MarkerId(markerIdVal);
-
-    final Marker marker = Marker(
-      markerId: markerId,
-      position: LatLng(51.67204, 39.1843),
-      infoWindow: InfoWindow(title: markerIdVal, snippet: '*'),
-      icon: BitmapDescriptor.fromBytes(resizedMarkerImageBytes),
-      onTap: () {
-        _onMarkerTapped(markerId);
-      },
-    );
-
-    setState(() {
-      // adding a new marker to map
-      markers[markerId] = marker;
-    });
-  }
-  
-  void _onMarkerTapped(markerId) {
-    print(markerId);
-  }
-
   @override
   void initState() {
     super.initState();
@@ -79,7 +31,7 @@ class _MapScreenState extends State<MapScreen> {
       Duration(seconds: 5),
       () {
         setState(() {
-          _add();
+          addMarker(markers);
         });
       }
     );
@@ -97,19 +49,7 @@ class _MapScreenState extends State<MapScreen> {
         },
         markers: Set<Marker>.of(markers.values),
         mapToolbarEnabled: false,
-
-        
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
+      )
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
